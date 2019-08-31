9e4546cdd9a9e78007d939479a203d2318d5f864
spartan1cs <aakashkumbhare@live.com>
2019-08-31 23:41:04 +0530

    Add dashboard link to the shell T

--- project1/src/app/app-routing.module.ts
+++ project1/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HerosComponent } from './heros/heros.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 
 const routes:Routes=[
-{ path:'heros', component:HerosComponent}
+{ path:'heros', component:HerosComponent},
+{ path: 'dashboard', component: DashboardComponent },
+
 ];
 
 @NgModule({
